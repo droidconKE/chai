d9c327423589a91a1df377e91cc035af21bed654
Kyle Roe <[hopeman15@users.noreply.github.com]>
2023-03-10 08:53:01 +0100

    settings

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = ("chai")
+include(":chai")
+include(":chaiswag")
+include(":chaidemo")
+include(":chailinter")
