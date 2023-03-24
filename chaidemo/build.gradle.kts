3c14bcb210d1c5425209e6aead98184747a94584
Kyle Roe <[hopeman15@users.noreply.github.com]>
2023-03-24 20:29:00 +0100

    Asset in action

--- chaidemo/build.gradle.kts
+++ chaidemo/build.gradle.kts
@@ -53,6 +53,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":chai"))
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
--- chaidemo/src/main/java/com/droidconke/chaidemo/ChaiDemoActivity.kt
+++ chaidemo/src/main/java/com/droidconke/chaidemo/ChaiDemoActivity.kt
@@ -3,12 +3,17 @@ package com.droidconke.chaidemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import com.droidconke.chai.assets.ChaiImage
 import com.droidconke.chaidemo.ui.theme.ChaidemoTheme
 
 class ChaiDemoActivity : ComponentActivity() {
@@ -21,7 +26,15 @@ class ChaiDemoActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Chai")
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            bitmap = ChaiImage.DCKE_MAIN(this@ChaiDemoActivity).asImageBitmap(),
+                            contentDescription = "",
+                        )
+                        Greeting("Chai")
+                    }
                 }
             }
         }
