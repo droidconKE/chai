0bd79bf9b67da6d964fd3074b4a139333635a7cf
tamzi <tamrefrank@gmail.com>
2023-04-20 21:58:06 +0200

    upgrade base

--- chaidemo/build.gradle.kts
+++ chaidemo/build.gradle.kts
@@ -68,19 +68,27 @@ android {
 }
 
 dependencies {
+    val composeBom = platform("androidx.compose:compose-bom:2023.04.01")
+    implementation(composeBom)
+
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
 
-    implementation("androidx.compose.ui:ui:1.4.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.1")
-    implementation("androidx.compose.material:material:1.4.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.1")
+    implementation("androidx.compose.ui:ui:1.4.2")
+
+    implementation("androidx.compose.ui:ui-graphics:1.4.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.2")
+    //implementation("androidx.compose.material:material:1.4.2")
+    implementation("androidx.compose.material3:material3:1.0.1")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.2")
 
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation(composeBom)
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.2")
 }
--- chaidemo/src/main/java/com/droidconke/chaidemo/ui/theme/Type.kt
+++ chaidemo/src/main/java/com/droidconke/chaidemo/ui/theme/Type.kt
@@ -16,7 +16,7 @@
 
 package com.droidconke.chaidemo.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -24,10 +24,12 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
+    bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     button = TextStyle(
