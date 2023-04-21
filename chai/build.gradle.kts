6186d131bf93b6a11633e47fa946bf31231185db
tamzi <tamrefrank@gmail.com>
2023-04-21 08:27:47 +0200

    Adding composeBom

--- chai/build.gradle.kts
+++ chai/build.gradle.kts
@@ -47,11 +47,14 @@ android {
 }
 
 dependencies {
+    val composeBom = platform("androidx.compose:compose-bom:2023.04.01")
+    implementation(composeBom)
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
 
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation(composeBom)
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
