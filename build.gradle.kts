50c01e7444b811e25be09abdd0ada77bfe5a1136
./root <tamrefrank@gmail.com>
2026-06-19 11:12:18 +0200

    Removed duplicate subprojects block from merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,19 +21,3 @@ subprojects {
         enabled = System.getenv("CI") != "true"
     }
 }
-
-// Apply performance optimizations to all subprojects
-subprojects {
-    tasks.withType<JavaCompile>().configureEach {
-        options.isIncremental = true
-    }
-
-    // Skip unnecessary tasks in CI
-    //See if i can add this to build Logic
-    tasks.matching {
-        it.name.contains("lint", ignoreCase = true) &&
-                it.name.contains("Baseline", ignoreCase = true)
-    }.configureEach {
-        enabled = System.getenv("CI") != "true"
-    }
-}
